1769273b0a02f861e33183e68bbf49431e6c878e
Caolán Maguire <36414229+calsickofthis@users.noreply.github.com>
2023-11-24 11:57:40 +0000

    problem sheet followed

---
+++ Lab8/lab_seven-main/src/components/bookItem.js
@@ -0,0 +1,28 @@
+import Card from 'react-bootstrap/Card';
+
+function BookItem(props) {
+
+    return (
+        <div>
+            <Card>
+                <Card.Header>{props.myBook.title}</Card.Header>
+                <Card.Body>
+                    <blockquote className="blockquote mb-0">
+                        <img src={props.myBook.cover}></img>
+                        <footer>
+                            {props.myBook.author
+                            
+                            }
+                        </footer>
+                    </blockquote>
+                </Card.Body>
+            </Card>
+            {/* <h3>{props.myBook.title}</h3>
+            <img src={props.myBook.thumbnailUrl}></img>
+            <p>{props.myBook.authors[0]}</p> */}
+        </div>
+    );
+
+}
+
+export default BookItem;
